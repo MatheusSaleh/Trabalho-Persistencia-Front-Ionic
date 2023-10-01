0879285b25184a5463a3232e5f887f4a1213a54b
MatheusSaleh <matheusxmg2@gmail.com>
2023-10-01 09:55:24 -0300

    Configurado o botao para marcar serviço como concluido

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -13,15 +13,33 @@ export class Tab3Page {
 
   public servicos: Servico[] = [];
 
+  public isModalAtualizarSituacaoDoServicoOpen: boolean = false;
+
+  public servicoSelecionadoParaAtualizarSituacao: Servico = new Servico();
+
   ngOnInit(): void{
     this.buscarInformacoesServicos();
   }
-
 
   public buscarInformacoesServicos(): void{
     this.apiService.getAllServicos().subscribe((dados: Servico[]) => {
       this.servicos = dados;
       console.log(dados)
+    })
+  }
+
+  public setOpenModalAtualizarSituacaoDoServico(isOpen: boolean, servicoSelecionado: Servico): void{
+    this.isModalAtualizarSituacaoDoServicoOpen = isOpen;
+    this.servicoSelecionadoParaAtualizarSituacao = servicoSelecionado;
+  }
+
+  public atualizarSituacaoDoServico(): void{
+    this.apiService.atualizarSituacaoDoServico(this.servicoSelecionadoParaAtualizarSituacao).subscribe(() => {
+      console.log("Serviço marcado como concluido com sucesso");
+      this.setOpenModalAtualizarSituacaoDoServico(false, this.servicoSelecionadoParaAtualizarSituacao);
+      this.buscarInformacoesServicos();
+    }, ()=> {
+      console.log("Erro ao atualizar situação do serviço")
     })
   }
 }
